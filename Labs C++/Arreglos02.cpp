6cf9b5a8b9ad7e53927b8efc22596f85f45a3881
Karina Meléndez <kmelendez@arecibo.inter.edu>
2024-04-30 09:55:58 -0400

    New File

---
+++ Labs C++/Arreglos02.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string> 
+using namespace std;
+
+int main()
+{
+    //Definir el arreglo con 5 nombres de ciudades o paises
+    string ciudades[5] = {"Paris", "Tokio", "Nueva York", "Londres", "Sydney"};
+
+    //Imprimir los nombres de las ciudades o paises
+    cout <<"Lista de ciudades o paises: " << endl;
+    for(int i = 0; i < 5; i++)
+    {
+        cout << "Ciudad/Pais " << i + 1 << ": " << ciudades[i] << endl;
+    }
+
+    return 0;
+}
